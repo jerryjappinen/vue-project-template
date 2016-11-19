a99bb6d52cc644dc5df32c7134c9f36a4722eeb2
Jerry Jäppinen <eiskis@gmail.com>
2016-11-19 21:25:35 +0100

    Libraries

--- source/js/plugins/fastclick.js
+++ source/js/plugins/fastclick.js
@@ -1,6 +1,6 @@
 
 // Bind FastClick
 // https://github.com/ftlabs/fastclick
-P.callbacks.fastclick = function (debug) {
-	FastClick.attach(window.document.body);
-};
+// P.callbacks.fastclick = function (debug) {
+// 	FastClick.attach(window.document.body);
+// };
