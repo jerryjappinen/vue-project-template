32ed639b9ebf40f8d797c50fa4a3223d4b65b284
Jerry Jäppinen <eiskis@gmail.com>
2016-11-19 13:33:06 +0100

    Swiper component tweaks

--- source/views/snippets/swiper/swiper.js
+++ source/views/snippets/swiper/swiper.js
@@ -3,7 +3,9 @@ V.views['swiper'] = {
 
 	props: [
 		'component',
-		'slides'
+		'slides',
+		'paginate',
+		'options'
 	],
 
 	data: function () {
@@ -25,14 +27,14 @@ V.views['swiper'] = {
 	// Life cycle
 
 	// Instantiate gallery
-	beforeCompile: function () {
+	mounted: function () {
 
 		// Swiper config
 		// http://idangero.us/swiper/api/
 		var options = {
 
 			// loop: true,
-			speed: 600,
+			speed: 300,
 			direction: 'horizontal',
 
 			// Slides that are in viewport will have additional visible class
@@ -54,30 +56,33 @@ V.views['swiper'] = {
 			// Class name of the wrapper element within the container
 			wrapperClass: 'swiper-wrapper',
 
-			// HTML
-			// Pagination
-			pagination: app.plugins.jQuery(this.$el).find('.swiper-pagination')[0],
-			paginationType: 'bullets',
-			// paginationHide: false,
-			// paginationElement: 'span',
-			// paginationClickable: true,
-			paginationBulletRender: function (index, className) {
-				return '<span class="' + className + '">' + (index + 1) + '</span>';
-			}
-
 		};
 
-		// l('Swiper compiled', this.$el, options);
+		if (this.paginate) {
+			_.merge(options, {
+
+				// HTML
+				// Pagination
+				pagination: this.$el.findElementsByClassName('.swiper-pagination')[0],
+				paginationType: 'bullets',
+				// paginationHide: false,
+				// paginationElement: 'span',
+				// paginationClickable: true,
+				paginationBulletRender: function (index, className) {
+					return '<span class="' + className + '">' + (index + 1) + '</span>';
+				}
+
+			});
+		}
+
+
+		if (this.options) {
+			_.merge(options, this.options);
+		}
 
 		this.swiperInstance = new Swiper(this.$el, options);
-	},
+		this.swiperInstance.update();
 
-	// Initiate Swiper correctly after rendering
-	mounted: function () {
-		if (this.swiperInstance) {
-			l('Swiper init');
-			this.swiperInstance.update();
-		}
 	},
 
 	beforeDestroy: function () {
